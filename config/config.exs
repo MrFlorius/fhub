2a34b566e16d355ea1ce93f2a989f5c8473204e4
vadimtsvetkov <vadim.tsvetkov80@gmail.com>
2021-02-05 19:44:59 +0300

    Configs fixed

--- config/config.exs
+++ config/config.exs
@@ -9,33 +9,25 @@
 # move said applications out of the umbrella.
 use Mix.Config
 
-config :fhub_web,
-  ecto_repos: [FhubWeb.Repo],
-  generators: [context_app: false]
+# config :fhub_web,
+#   ecto_repos: [FhubWeb.Repo],
+#   generators: [context_app: false]
 
-# Configures the endpoint
-config :fhub_web, FhubWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "n5l2bdhS8JrMK2h0c1dF9FQjTeBULGZ5vONnfeCB98LpUMUnzpx9YlpWMb8jpZsp",
-  render_errors: [view: FhubWeb.ErrorView, accepts: ~w(html json), layout: false],
-  pubsub_server: FhubWeb.PubSub,
-  live_view: [signing_salt: "WaJ1tZtz"]
+# # Configures the endpoint
+# config :fhub_web, FhubWeb.Endpoint,
+#   url: [host: "localhost"],
+#   secret_key_base: "n5l2bdhS8JrMK2h0c1dF9FQjTeBULGZ5vONnfeCB98LpUMUnzpx9YlpWMb8jpZsp",
+#   render_errors: [view: FhubWeb.ErrorView, accepts: ~w(html json), layout: false],
+#   pubsub_server: FhubWeb.PubSub,
+#   live_view: [signing_salt: "WaJ1tZtz"]
 
 # Configure Mix tasks and generators
 config :fhub,
   ecto_repos: [Fhub.Repo]
 
-config :fhub_web,
-  ecto_repos: [Fhub.Repo],
-  generators: [context_app: :fhub, binary_id: true]
-
-# Configures the endpoint
-config :fhub_web, FhubWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "Wa6jdyOLmWQ8u73nAMNN6sGcBksIj2Q977oTlLeDywc7O+1CGWBoHuArNjDFZqZD",
-  render_errors: [view: FhubWeb.ErrorView, accepts: ~w(html json), layout: false],
-  pubsub_server: Fhub.PubSub,
-  live_view: [signing_salt: "9diS8EXv"]
+# config :fhub_web,
+#   ecto_repos: [Fhub.Repo],
+#   generators: [context_app: :fhub, binary_id: true]
 
 # Configures Elixir's Logger
 config :logger, :console,
--- config/test.exs
+++ config/test.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :fhub_web, FhubWeb.Endpoint,
-  http: [port: 4002],
-  server: false
+# config :fhub_web, FhubWeb.Endpoint,
+#   http: [port: 4002],
+#   server: false
 
 # Configure your database
 #
@@ -17,12 +17,6 @@ config :fhub, Fhub.Repo,
   database: "fhub_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
-
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
-config :fhub_web, FhubWeb.Endpoint,
-  http: [port: 4002],
-  server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn

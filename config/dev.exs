2fe8a331d1b91b12c9b5242a684ddf6be80566dc
vadimtsvetkov <vadim.tsvetkov80@gmail.com>
2021-02-05 19:46:04 +0300

    Config fixed #2

--- config/dev.exs
+++ config/dev.exs
@@ -69,8 +69,8 @@ config :fhub, Fhub.Repo,
 config :logger, :console, format: "[$level] $message\n"
 
 # Initialize plugs at runtime for faster development compilation
-config :phoenix, :plug_init_mode, :runtime
+# config :phoenix, :plug_init_mode, :runtime
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
-config :phoenix, :stacktrace_depth, 20
+# config :phoenix, :stacktrace_depth, 20

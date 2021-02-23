caa517ef276f7b7e32535f6e1b28b98b9014eb6a
vadimtsvetkov <vadim.tsvetkov80@gmail.com>
2021-02-24 01:45:01 +0300

    Unique App names

--- apps/fhub/lib/fhub/apps.ex
+++ apps/fhub/lib/fhub/apps.ex
@@ -10,6 +10,30 @@ defmodule Fhub.Apps do
   use Fhub.AccessControl.Context, for: App
 
   # TODO: Enforce unique Apps names
+  def update_app(app = %App{}, attrs, actor) do
+    transaction = fn repo, _ ->
+      app
+      |> repo.preload([:resource])
+      |> change_app_update(attrs)
+      |> (fn c ->
+            %{name: name} = Ecto.Changeset.apply_changes(c)
+            %{resource: %{id: id, parent_id: parent_id}} = app
+
+            cast_resource_for_app(c, %{id: id, parent_id: parent_id, name: name})
+          end).()
+      |> repo.update()
+    end
+
+    Fhub.AccessControl.Transactions.operation(transaction, actor, :update)
+  end
+
+  def build_resource_for_app(parent, _actor, changeset) do
+    %{name: name} = Ecto.Changeset.apply_changes(changeset)
+    r = Fhub.Resources.ResourceProtocol.resource(parent)
+
+    %{parent_id: r.id, name: name}
+  end
+
   def list_functions(%App{} = app, actor) do
     q =
       from f in Function,
